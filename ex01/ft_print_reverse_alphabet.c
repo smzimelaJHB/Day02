99336f0a864474c0e6ab0885d0cf53bf36a601e5
Siyabonga <smzimela@student.wethinkcode.co.za>
2020-06-18 19:29:51 +0200

    ex 01 without a.out

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+  write(1,&c,1);
+}
+
+void ft_print_reverse_alphabet();
+
+int main()
+{
+        ft_print_reverse_alphabet();
+        return 0;
+}
+
+void ft_print_reverse_alphabet()
+{
+        char ch  = 'z';
+        while(ch >= 'a')
+        {
+                ft_putchar(ch);
+                ch--;
+        }
+}
